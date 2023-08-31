874a0709fe9191e01505ea4797a1b7c508c4b17e
Jonatan asd <jawntalol@gmail.com>
2023-08-31 11:09:32 +0200

    refactored homeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,38 @@
+<template>
+  <div class="wrapper"> 
+    <div class="trainList">
+      <trainsTable />
+    </div>
+    <div class="map">
+      <mapComponentVue />
+    </div>
+
+
+
+</div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import mapComponentVue from '../components/mapComponent.vue'
+  import trainsTable from '../components/trainsTable.vue'
+  import { ref } from 'vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-flow: row;
+  text-align: center;
+}
+.map {
+  display: flex;
+  height: auto;
+  width: 100%;
+  padding-left: 5px;
+}
+.trainList {
+  width: 40%;
+  padding-right: 5px;
+}
+
+</style>
