b7aa2f4e8706d77b35d3dd9392b9f56781f51b36
Andreas Nilsson <andreas.nilsson8905@gmail.com>
2023-09-08 14:28:29 +0200

    changed order

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-        baseUrl: 'http://localhost:4173/~jorp21/editor/'
+        baseUrl: 'http://localhost:4173'
+    },
+    env: {
+        BACKEND: 'https://jsramverk-editor-jorp.azurewebsites.net'
     }
 })
--- frontend/cypress/e2e/delayedTrainsTable.spec.js
+++ frontend/cypress/e2e/delayedTrainsTable.spec.js
@@ -12,31 +12,21 @@ describe('Delayed Trains Component Tests', () => {
     ]
 
     beforeEach(() => {
-        cy.request(`https://jsramverk-editor-jorp.azurewebsites.net/delayed`).its('status').should('eq', 200);
-        // Intercepting the API call to return mock data and aliasing it.
-        cy.intercept('GET', `https://jsramverk-editor-jorp.azurewebsites.net/delayed`, {
+        // Intercepting the API call to return mock data.
+        cy.intercept('GET', `${Cypress.env('BACKEND')}/delayed`, {
             body: { data: delayedTrainsData }
-        }).as('fetchDelayedTrains')
-
+        })
         cy.visit('/')
     })
 
     it('should display delayed trains correctly', () => {
-        cy.wait('@fetchDelayedTrains') // Wait for the intercepted request
-
-        cy.get('.delayed .train-number', { timeout: 10000 }) // 10 seconds timeout
-            .first()
-            .should('contain', '12345')
-        cy.get('.delayed .current-station', { timeout: 10000 })
-            .first()
-            .should('contain', 'Station A')
-        cy.get('.delayed .delay', { timeout: 10000 }).first().should('contain', '15 minuter')
+        cy.get('.delayed .train-number').first().should('contain', '12345')
+        cy.get('.delayed .current-station').first().should('contain', 'Station A')
+        cy.get('.delayed .delay').first().should('contain', '15 minuter')
     })
 
     it('should navigate to ticket view on clicking a train row', () => {
-        cy.wait('@fetchDelayedTrains') // Wait for the intercepted request
-
-        cy.get('.delayed tbody tr', { timeout: 10000 }).first().click()
+        cy.get('.delayed tbody tr').first().click()
         cy.url().should('include', '/details/12345')
     })
 })
