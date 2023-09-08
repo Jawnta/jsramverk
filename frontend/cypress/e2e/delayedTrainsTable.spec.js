e4f5f99135427f07cd7dbbe4ea7f83e2c9dafb15
Andreas Nilsson <andreas.nilsson8905@gmail.com>
2023-09-08 14:22:32 +0200

    removed env

--- frontend/cypress/e2e/delayedTrainsTable.spec.js
+++ frontend/cypress/e2e/delayedTrainsTable.spec.js
@@ -12,9 +12,9 @@ describe('Delayed Trains Component Tests', () => {
     ]
 
     beforeEach(() => {
-        cy.request(`${Cypress.env('BACKEND')}/delayed`).its('status').should('eq', 200);
+        cy.request(`https://jsramverk-editor-jorp.azurewebsites.net/delayed`).its('status').should('eq', 200);
         // Intercepting the API call to return mock data and aliasing it.
-        cy.intercept('GET', `${Cypress.env('BACKEND')}/delayed`, {
+        cy.intercept('GET', `https://jsramverk-editor-jorp.azurewebsites.net/delayed`, {
             body: { data: delayedTrainsData }
         }).as('fetchDelayedTrains')
 
