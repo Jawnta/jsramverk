37508558f806d5de804fb9b3043a5b3cd42917ed
Andreas Nilsson <andreas.nilsson8905@gmail.com>
2023-09-12 11:18:22 +0200

    url change

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-        baseUrl: 'http://localhost:4173'
+        baseUrl: 'http://localhost:4173/~jorp21/editor/'
     },
     env: {
         BACKEND: 'https://jsramverk-editor-jorp.azurewebsites.net'
