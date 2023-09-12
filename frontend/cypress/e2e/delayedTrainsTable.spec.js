59864fe2dcae6c8e018843950245e2df907ad26b
NilssonAndreas <84865109+NilssonAndreas@users.noreply.github.com>
2023-09-12 13:16:10 +0200

    Update delayedTrainsTable.spec.js

--- frontend/cypress/e2e/delayedTrainsTable.spec.js
+++ frontend/cypress/e2e/delayedTrainsTable.spec.js
@@ -1,25 +1,47 @@
 describe('Delayed Trains Component Tests', () => {
 
     beforeEach(() => {
-        // Load the fixture and set up the intercept
+        cy.visit('/'); // Assuming this is the correct URL for your component
+    });
+
+    it('should render the table headers correctly', () => {
+        cy.get('table thead th').eq(0).should('contain', 'Tågnummer');
+        cy.get('table thead th').eq(1).should('contain', 'Nuvarande station');
+        cy.get('table thead th').eq(2).should('contain', 'Försening');
+    });
+
+    it('should display specific train data correctly', () => {
+        // Intercept here
         cy.fixture('delayedTrains.json').then((delayedTrainsData) => {
             cy.intercept('GET', `${Cypress.env('BACKEND')}/delayed`, {
                 body: { data: delayedTrainsData }
             }).as('delayedTrains');
         });
+
+        // Reload or perform an action that triggers the request
         cy.visit('/');
         cy.wait('@delayedTrains');
 
-    });
-
-    it('should display delayed trains correctly', () => {
-        cy.get('[data-testid="train-number"]').first().should('contain', '12345');
+        cy.get('[data-testid="train-number"]').first().should('contain', '12345'); // Use your fixture data for assertions
         cy.get('[data-testid="current-station"]').first().should('contain', 'Station A');
         cy.get('[data-testid="delay"]').first().should('contain', '15 minuter');
     });
 
     it('should navigate to ticket view on clicking a train row', () => {
-        cy.get('[data-testid="train-row-12345"]').first().click();
-        cy.url().should('include', '/details/12345');
+        // Intercept here if needed
+        cy.fixture('delayedTrains.json').then((delayedTrainsData) => {
+            cy.intercept('GET', `${Cypress.env('BACKEND')}/delayed`, {
+                body: { data: delayedTrainsData }
+            }).as('delayedTrains');
+        });
+
+        // Reload or perform an action that triggers the request
+        cy.visit('/');
+        cy.wait('@delayedTrains');
+
+        cy.get('[data-testid^="train-row-"]').first().click();
+        cy.url().should('include', '/details/12345'); // Use your fixture data for assertions
     });
+
+    // ... other tests
 });
